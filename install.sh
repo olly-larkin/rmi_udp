bd37924ec729db6b5d50dc21d44aabdfc2232508
Olly Larkin <ollylarkin@dyn3196-214.wlan.ic.ac.uk>
2019-02-05 12:29:09 +0000

    install complete

--- install.sh
+++ install.sh
@@ -6,10 +6,6 @@ echo \(c\) Daniele Sgandurra, Imperial College London, Mar 2016
 rm -f rmiclient.bat rmiserver.bat udpclient.bat udpserver.bat build.bat 
 cp scripts/Makefile .
 cp scripts/*.sh .
-fromdos policy
-for i in *.sh; do
-    fromdos $i
-done
 chmod u+x *.sh
 
 echo Done!
